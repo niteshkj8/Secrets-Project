38b30dcf40e4a80c0cf29ba89a3c1ef3b0d84ac7
Nitesh Kumar Jaiswal <niteshkj8@gmail.com>
2024-08-09 19:11:45 +0530

    First version

---
+++ index.js
@@ -0,0 +1,26 @@
+
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com";
+
+app.use(express.static("public"));
+
+app.get("/", async(req,res) => {
+    try{
+        const result = await axios.get(API_URL + "/random");
+        res.render("index.ejs", {
+            secret: result.data.secret,
+            user: result.data.username,
+        });
+    }
+    catch(error){
+        res.render("index.ejs",{ error: error.response.data });
+    }
+});
+
+app.listen(port, () =>{
+    console.log(`Server is Running on port ${port}`);
+});
